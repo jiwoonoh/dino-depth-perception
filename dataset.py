23b061ace3a46ce67e00c652bb8aebcc3df16205
jiwoo <jiwoonoh1@gmail.com>
2024-12-09 03:10:10 -0600

    update

--- dataset.py
+++ dataset.py
@@ -23,3 +23,25 @@ class DictDataset(Dataset):
         file_path = self.file_paths[idx]
         data = torch.load(file_path)
         return data
+
+class RepeatedDictDataset(Dataset):
+    def __init__(self, file_path, dataset_size):
+        """
+        Args:
+            file_path (str): Path to the .pt file.
+            dataset_size (int): Number of times to repeat the file.
+        """
+        self.file_path = file_path
+        self.dataset_size = dataset_size
+
+    def __len__(self):
+        # The dataset has a fixed size
+        return self.dataset_size
+
+    def __getitem__(self, idx):
+        """
+        Load and return the dictionary from the .pt file.
+        Repeats the same file regardless of the index.
+        """
+        data = torch.load(self.file_path)
+        return data
